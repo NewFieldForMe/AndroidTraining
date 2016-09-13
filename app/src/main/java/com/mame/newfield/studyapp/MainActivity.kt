a2a96470f9db04c2c84964fe4de9bd7f216fcb9c
RyoYamada <newfield1001@gmail.com>
2016-09-13 13:29:35 +0900

    test button event

--- app/src/main/java/com/mame/newfield/studyapp/MainActivity.kt
+++ app/src/main/java/com/mame/newfield/studyapp/MainActivity.kt
@@ -2,11 +2,27 @@ package com.mame.newfield.studyapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
+    //region < initializer >
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button) as Button
+        button.setOnClickListener { view ->
+            buttonOnClick(view)
+        }
     }
+    //endregion
+    
+    //region < control event >
+    fun buttonOnClick(v: View?) {
+        Toast.makeText(this, "Tapped", Toast.LENGTH_SHORT).show()
+    }
+    //endregion
 }
